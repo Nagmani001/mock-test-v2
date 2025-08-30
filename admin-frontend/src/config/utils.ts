66b129e608f0214b9c55a03d929080eccb6c28fe
nagmani <nagmanipd3@gmail.com>
2025-08-30 16:40:08 +0545

    Asdf

--- admin-frontend/src/config/utils.ts
+++ admin-frontend/src/config/utils.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://be.mocktest.nagmaniupadhyay.com.np/api/v1/admin"
- export const BASE_URL = "http://localhost:3000/api/v1/admin"
+ export const BASE_URL = "https://be.mocktest.nagmaniupadhyay.com.np/api/v1/admin"
+// export const BASE_URL = "http://localhost:3000/api/v1/admin"
--- frontend/src/config/utils.ts
+++ frontend/src/config/utils.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000"
-// export const BASE_URL = "https://be.mocktest.nagmaniupadhyay.com.np"
+// export const BASE_URL = "http://localhost:3000"
+ export const BASE_URL = "https://be.mocktest.nagmaniupadhyay.com.np"
 export const tests = [{
   title: "Descriptive Paper For IBPS PO 2025 Test-1 (New Pattern)",
   language: "English",
